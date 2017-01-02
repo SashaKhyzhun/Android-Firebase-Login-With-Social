e8dc12925e994bc90a5fb4ad62094ddf3a301647
SashaKhyzhun <sasha.khyzhun@gmail.com>
2017-01-02 22:51:05 +0200

    add all my files

---
+++ app/src/main/java/com/sashakhyzhun/android_firebase_login_with_social/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sashakhyzhun.android_firebase_login_with_social;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
